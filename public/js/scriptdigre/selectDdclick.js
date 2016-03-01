bc7df81224f212981c72ac26b6ef5305ea6c2b96
Facundo <digredeth@gmail.com>
2016-03-01 20:38:09 -0300

    se

--- public/js/scriptdigre/selectDdclick.js
+++ public/js/scriptdigre/selectDdclick.js
@@ -1,9 +1,9 @@
  $(document).ready(function(){
  	$(".row-data").dblclick(function(){
- 		$(this).find("td").find(".cod",(function(){
-           $val=$(this).text();
-           console.log($val);
+ 		$codigo=$(this).find(".codigo").text();
+ 		$descripcion=$(this).find(".descripcion").text();
+         console.log($codigo+" "+$descripcion);
+         $("#ListaPed").append("<li class=\"list-group-item\"><span class=\"badge col-xs-2\"><input class=\"form-control input-sm\" type=\"number\" min=1 id=\"inputSmall\"></span>"+$descripcion+"</li>");
  		})
  	})
 
- })
--- resources/views/principalMovimientos.blade.php
+++ resources/views/principalMovimientos.blade.php
@@ -6,69 +6,54 @@
 	<!--Columna tabla articulos-->
 	<div class="col-md-6">
 		<!-- Your first column here -->
-        <div class="panel panel-primary">
-        	<div class="panel-heading">
-        		<h3 class="panel-title">Articulos</h3>
-        	</div>
-        	<div class="panel-body">
-        		<div style="height:600px;overflow-y:scroll;">
-			<table id="tart" class="table table-bordered" >
-				<thead >
-					<tr>
-						<th>Codigo</th>
-						<th>Descripcion</th>
-						<th>Proveedor</th>
-						<th>Stock</th>
-						<th>Limite</th>
-					</tr>
-				</thead>
-				@foreach($articulos as $articulo)
-				<tbody>
-					<tr class="row-data">
-						<td id="codigo">{{$articulo->cod}}</td>
-						<td class="descripcion">{{$articulo->artdesc}}</td>
-						<td>{{$articulo->provdesc}}</td>
-						<td>{{$articulo->stock}}</td>
-						<td>{{$articulo->limite}}</td>
-					</tr>
-				</tbody>
-				@endforeach
-			</table>
+		<div class="panel panel-primary">
+			<div class="panel-heading">
+				<h3 class="panel-title">Articulos</h3>
+			</div>
+			<div class="panel-body">
+				<div style="height:600px;overflow-y:scroll;">
+					<table id="tart" class="table table-hover" >
+						<thead >
+							<tr>
+								<th>Codigo</th>
+								<th>Descripcion</th>
+								<th>Proveedor</th>
+								<th>Stock</th>
+								<th>Limite</th>
+							</tr>
+						</thead>
+						@foreach($articulos as $articulo)
+						<tbody>
+							<tr class="row-data">
+								<td class="codigo">{{$articulo->cod}}</td>
+								<td class="descripcion">{{$articulo->artdesc}}</td>
+								<td>{{$articulo->provdesc}}</td>
+								<td>{{$articulo->stock}}</td>
+								<td>{{$articulo->limite}}</td>
+							</tr>
+						</tbody>
+						@endforeach
+					</table>
+				</div>
+			</div>
 		</div>
-        	</div>
-        </div>
 		
 	</div>
 	<!-- Columna lista articulos agregados-->
 	<div class="col-md-6">
 		<!-- Your second column here -->
-        <div class="panel panel-success">
-        	<div class="panel-heading">
-        		<h3 class="panel-title">Pedido Articulos</h3>
-        	</div>
-        	<div class="panel-body">
-        		<ul class="list-group">
-			<li class="list-group-item">
-				<span class="badge">14</span>
-				Cras justo odio
-			</li>
-			<li class="list-group-item">
-				<span class="badge">2</span>
-				Dapibus ac facilisis in
-			</li>
-			<li class="list-group-item">
-				<span class="badge">1</span>
-				Morbi leo risus
-			</li>
-		</ul>
-        	</div>
-        </div>
+		<div class="panel panel-success">
+			<div class="panel-heading">
+				<h3 class="panel-title">Pedido Articulos</h3>
+			</div>
+			<div class="panel-body">
+				<ul id="ListaPed" class="list-group">
+				</ul>
+			</div>
+		</div>
 		
 	</div>
 </div>
-	@endsection
-
+@endsection
 {!!Html::script('js/jquery.js')!!}
-
 {!!Html::script('js/scriptdigre/selectDdclick.js')!!}
-
